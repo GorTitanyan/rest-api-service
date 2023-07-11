eecd7c5174340dbb132d2a71c669fbaedbff17fd
gor <got.titanyan2006@gmail.com>
2023-07-11 15:49:19 +0400

    add sql schema structure

--- db_structure.sql
+++ db_structure.sql
@@ -1,14 +1,19 @@
-USE infoHub; -- Replace 'your_database_name' with the actual name of your database
+CREATE SCHEMA `infoHub`;
+USE infoHub;
 
--- CREATE TABLE `users` (
---   `id` int NOT NULL AUTO_INCREMENT,
---   `email` varchar(255) DEFAULT NULL,
---   `password` varchar(255) NOT NULL,
---   `phone_number` varchar(20) DEFAULT NULL,
---   PRIMARY KEY (`id`),
---   UNIQUE KEY `email` (`email`),
---   UNIQUE KEY `phone_number_UNIQUE` (`phone_number`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `surname` varchar(25) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `birthday` date NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `tokens` (
   `id` int NOT NULL,
